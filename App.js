0781eb677b834a066f9329b9aeafe5aeed516b06
Tiago da Silva Evaristo <63016462+TiagoEvaristo@users.noreply.github.com>
2024-05-11 14:58:25 -0300

    Finished tutorial

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import * as ImagePicker from 'expo-image-picker';
+import * as MediaLibrary from 'expo-media-library';
+import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
@@ -47,16 +51,57 @@ export default function App() {
 
   const onSaveImageAsync = async () => {
     // we will implement this later
+    if(Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+
+        await MediaLibrary.saveToLibraryAsync(localUri);
+
+        if(localUri) {
+          alert('Saved!');
+        }
+      }
+      catch(error) {
+        console.log(error);
+      }
+    }else{
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current,{
+          quality: 1,
+          width: 320,
+          height: 440,
+        });
+        
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+
+  if(status === null) {
+    requestPermission();
+  }
+
+  const imageRef = useRef();
+
+
   return (
-    <View style={styles.container}>
-      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
-      </EmojiPicker>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
-        {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+        <View ref={imageRef} collapsable={false}> 
+          <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
+          {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+        </View>
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
@@ -75,8 +120,11 @@ export default function App() {
           />
         </View>
       )}
-      <StatusBar style="auto" />
-    </View>
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
+      <StatusBar style="light" />
+    </GestureHandlerRootView>
   );
 }
 
